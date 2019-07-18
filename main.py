e744dd4674273d96039ebf293a8657fe0a697ec4
Noah <ndangar1@students.kennesaw.edu>
2019-07-18 18:30:20 -0400

    Main File created.
    Basic python file, check 1

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+from __future__ import print_function
+
 
 ## Below are the rudimentary controls for the variables.
-faithfulness = 0
-generations = 0
+faithfulness = 4
+generations = 4
 transformation = 0
+
+
+## Building a basic text network
+
+for gen in range(generations):
+
+    print("\n|")
+    ## print(gen)
+
+    for mark in range(gen + 1):
+        
+        for faith in range(faithfulness):
+            print("x", end = ' ')
+   
